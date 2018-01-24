d743a408ec6371c49f2f6b30f59883af1e06741c
rh <ronny.hildebrandt@avorium.de>
2018-01-24 20:23:03 +0100

    Extended authenticate to include modules and write option.

--- src/server/api/users.ts
+++ src/server/api/users.ts
@@ -1,11 +1,13 @@
 import { App } from "../app";
-import { Auth } from "../utils/auth";
+import { Auth, LoggedInUserRequest } from "../utils/auth";
+import { Module } from "../utils/module";
+import { Db } from "../utils/db";
 
 export default () => {
 
-    App.router.get('/users', Auth.authenticate(), async (req, res) => {
-        console.log("USERS");
-        res.send("USERS");
+    App.router.get('/users', Auth.authenticate(Module.Users, false), async (req: LoggedInUserRequest, res) => {
+        let users = (await Db.query(req.loggedInUser!.databaseName, "select name from users")).rows;
+        res.send(users);
     });
 
 }
--- src/server/utils/auth.ts
+++ src/server/utils/auth.ts
@@ -2,14 +2,24 @@ import { Db } from "./db";
 import { hashSync, compareSync } from "bcryptjs";
 import { Request, Response, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
+import { Module, getModuleName } from "./module";
 
 export interface AuthenticatedUser {
     databaseName: string;
     username: string;
 }
 
-export interface AuthenticatedUserRequest extends Request {
-    authenticatedUser?: AuthenticatedUser;
+export interface LoggedInUser extends AuthenticatedUser {
+    permissions: LoggedInUserPermission[];
+}
+
+export interface LoggedInUserPermission {
+    permission: string;
+    write: boolean;
+}
+
+export interface LoggedInUserRequest extends Request {
+    loggedInUser?: LoggedInUser;
 }
 
 /**
@@ -20,13 +30,22 @@ export class Auth {
     /**
      * Extracts the token from the request and authenticates the user
      */
-    static authenticate() {
-        return async (req: AuthenticatedUserRequest, res: Response, next: NextFunction) => {
+    static authenticate(module: Module | null, needWrite: boolean) {
+        return async (req: LoggedInUserRequest, res: Response, next: NextFunction) => {
             let token = req.headers["x-access-token"];
             if (!token) return res.sendStatus(401);
-            verify(token as string, "hubbelebubbele", (err, decoded) => {
+            verify(token as string, "hubbelebubbele", async (err, decoded) => {
                 if (err) return res.sendStatus(401);
-                req.authenticatedUser = decoded as AuthenticatedUser;
+                // Delete jsonwebtoken stuff
+                delete (decoded as any).iat;
+                delete (decoded as any).exp;
+                let user = decoded as LoggedInUser;
+                user.permissions = (await Db.query(user.databaseName, "select usergrouppermissions.permission, usergrouppermissions.write from users join usergroups on users.usergroup = usergroups.name join usergrouppermissions on usergrouppermissions.usergroup = usergroups.name where users.name = '" + user.username + "'")).rows as LoggedInUserPermission[];
+                if (module) {
+                    let moduleName = getModuleName(module);
+                    if (!user.permissions.find(p => p.permission === moduleName && (!needWrite || p.write))) return res.sendStatus(403);
+                }
+                req.loggedInUser = user;
                 next();
             });
         }
